0cd7ebe1b1c4a6a562d049704fbbd2aa530c0aeb
dBobrivnyk <hoxtber@gmail.com>
2021-03-06 17:51:23 +0200

    Unity project init

---
+++ DeathFrom/Assets/Scripts/MineFieldVisual.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MineFieldVisual : MonoBehaviour
+{
+    // [Header("Ground Tile Texture")]
+    // //Choose random tile
+    // [SerializeField] private TileBase tileToSet;
+    
+    // private Tilemap map;
+
+    // void Start()
+    // {
+    //     map = gameObject.transform.GetComponentInChildren<Tilemap>();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    // public void UpdateGroundVisual(int rangeX, int rangeY)
+    // {
+    //     for(int x=0;x<rangeX;x++)
+    //         {
+    //             for(int y=0;y<rangeY;y++)
+    //             {
+    //                 map.SetTile(new Vector3Int(x,y,0),tileToSet);
+    //             }
+    //         }
+    // }
+}
